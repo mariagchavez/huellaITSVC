0286edcc82333efe768c0c290ea45f697db71954
maria <chavezguadalupe1503@gmail.com>
2025-03-04 17:25:15 -0600

    Cambios de diseño, actualizaciones

---
+++ assets/dataTables/dataTables.js
@@ -0,0 +1,20 @@
+$('#dataTableUsuarios').DataTable({
+    language: {
+      url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+    }
+});
+$('#dataTableTec').DataTable({
+  language:{
+    url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+  }
+});
+$('#dataTableEventos').DataTable({
+  language: {
+    url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+  }
+});
+$('#dataTableAddEventos').DataTable({
+  language: {
+    url: "https://cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
+  }
+});
